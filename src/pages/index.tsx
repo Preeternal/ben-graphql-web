acacb05f1c3b86693f02fe0b8b39a977091a8750
Preeternal <preeternal@ya.ru>
2020-10-23 15:16:34 +0300

    styling posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,51 @@
 import React from 'react';
 import { withUrqlClient } from 'next-urql';
-import { Box, Heading, Link, Stack, Text } from '@chakra-ui/core';
+import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
 import NextLink from 'next/link';
 import { createUrqlClient } from '../utils/createUrqlClients';
 import { usePostsQuery } from '../generated/graphql';
 import { Layout } from '../components/Layout';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 15,
       // cursor: null,
     },
   });
+  if (!fetching && !data) {
+    return <div>you got query failed for some reason</div>;
+  }
   return (
     <Layout>
-      <NextLink href="/create-post">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <Link>create post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <Link ml="auto">create post</Link>
+        </NextLink>
+      </Flex>
 
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data.posts.map(p => (
+          {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
+          {data!.posts.map(p => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
             </Box>
           ))}
         </Stack>
+      )}
+      {data && (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={8}>
+            load more
+          </Button>
+        </Flex>
       )}
     </Layout>
   );
